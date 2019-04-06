b31da77b1ece39bcaebacdb7d8d6952802489a9b
Ashley Owens <ash4741@gmail.com>
2019-04-06 13:22:52 -0400

    Add myToken Basic Info

---
+++ auth/myTokens.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = {
+  generateToken,
+};
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '1d',
+  };
+
+  return jwt.sign(payload, secrets, options);
+};
